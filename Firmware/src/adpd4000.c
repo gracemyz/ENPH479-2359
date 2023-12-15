2f869640ebb297917f85e5a52e008a8ba782fb93
Grace Zhang <gracemyzhang@gmail.com>
2023-12-15 13:54:20 -0800

    Implement spi.receive function

--- Firmware/src/adpd4000.c
+++ Firmware/src/adpd4000.c
@@ -3,10 +3,21 @@
 #include <stdint.h>
 #include <SPI.h>
 
+/* Bluepill pin values and settings */
+#define BP_LED_BUILTIN PC13 // Bluepill built-in LED
+#define BP_DATA_OUT PA7 // Bluepill MOSI 1
+#define BP_DATA_IN PA6 // Bluepill MISO 1
+#define BP_SCK PA6 // Bluepill SCK 1
+#define BP_NSS PA5 // Bluepill Chip Select 1
 
+/* ADI */
 #define ADI_OK      0
 #define ADI_ERROR  -1
 
+/**
+ * Including this because sometimes uint16_t doesn't get recognized as a type
+ * even though stdint.h is included... 
+ */
 #ifndef __STDINT_H_
     typedef int int16_t;
     typedef unsigned int uint16_t;
@@ -18,23 +29,31 @@
 #define datamode SPI_MODE0 // Check this!!!!
 /**
  * @brief    function to write a byte and read specified number of bytes from Adpd400 SPI
- * @param    pTxData: 8-bit register address pointer
- * @param    pRxData: 8-bit data pointer
+ * @param    pTxData: 8-bit register address pointer. Array of 2 bytes
+ * @param    pRxData: 8-bit data pointer. Array of 4 bytes
  * @param    TxSize: 16-bit size of the data to write to the hardware
  * @param    RxSize: 16-bit size of the data to read from the hardware
  * @retval   status: 0 (Success), -1 (Error)
  */
-uint16_t Adpd400x_SPI_Receive(uint8_t *pTxData, uint8_t *pRxData, 
-                              uint16_t TxSize, uint16_t RxSize) {
-    // uint16_t cnt = 0;
+uint16_t Adpd400x_SPI_Receive(uint8_t *pTxData, uint8_t *pRxData, uint16_t TxSize, uint16_t RxSize) {
+    SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
+    digitalWrite(BP_NSS, LOW); //enable device
+
+    // Write register address and READ command
+    uint16_t buffer = (pTxData[0] << 8) | (pTxData[1]);
+    SPI.transfer(&buffer, (int)TxSize);
+
+    // The SPI protocol is based on a one byte OUT / one byte IN interface. 
+    // For every byte expected to be received, one (dummy, typically 0x00 or 0xFF) byte must be sent.
+    for (int i=0; i < (int)RxSize; i++) {
+        pRxData[i] = SPI.transfer(0x00);
+    }
     
-    // cnt = spi.write((const char*)pTxData, (int)TxSize, 
-    //                 (char *)pRxData, (int)RxSize);
-    // spi_cs = 1;
-    
-    // serial_to_pc.printf("%u\r\n", cnt);
-    
-    // return ((TxSize + RxSize) == cnt)?ADI_HAL_OK:ADI_HAL_ERROR;
+    digitalWrite(BP_NSS, HIGH);
+    SPI.endTransaction();
+
+
+    return ((TxSize + RxSize) == sizeof(pTxData) + sizeof(pRxData))?ADI_OK:ADI_ERROR;
 
 }
 
