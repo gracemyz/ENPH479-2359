959fa47e2e96f229c004cf5255222b6aacb99c16
gracemyz <gracemyzhang@gmail.com>
2024-01-19 13:33:49 -0800

    Restructure

--- Firmware/lib/adi-adpd-library/adpd4000.cpp
+++ Firmware/lib/adi-adpd-library/adpd4000.cpp
@@ -1,73 +1,80 @@
 
-// #include "adpd4000.h"
-// #include <stdint.h>
-// #include <SPI.h>
+#include <SPI.h>
+#include "adi_adpd_driver.h"
+#include "adi_adpd_reg.h"
+#include "adi_adpd_reg.h"
+#include "adpd4000.h"
+#include "Arduino.h"
+#include "stdint.h" 
 
-// /* Bluepill pin values and settings */
-// #define BP_LED_BUILTIN PC13 // Bluepill built-in LED
-// #define BP_DATA_OUT PA7 // Bluepill MOSI 1
-// #define BP_DATA_IN PA6 // Bluepill MISO 1
-// #define BP_SCK PA6 // Bluepill SCK 1
-// #define BP_NSS PA5 // Bluepill Chip Select 1
 
-// /* ADI */
-// #define ADI_OK      0
-// #define ADI_ERROR  -1
 
-// /**
-//  * Including this because sometimes uint16_t doesn't get recognized as a type
-//  * even though stdint.h is included... 
-//  */
-// // #ifndef __STDINT_H_
-// //     typedef int int16_t;
-// //     typedef unsigned int uint16_t;
-// // #endif
+/**
+ * @brief Blink the bluepill's builtin LED a number of times
+ * @param num_blinks 
+ * @retval none
+ */
+void blink_bp(int num_blinks) {
+  for (int i=0; i < num_blinks; i++) {
+    delay(BLINK_DUR_ms);
+    digitalWrite(BP_LED_BUILTIN, LOW);
+    delay(BLINK_DUR_ms);
+    digitalWrite(BP_LED_BUILTIN, HIGH);
+  }
+}
 
-// /* ADPD4000 SPI settings */
-// #define maxspeed 24000000 // ADPD4000 sclk frequency
-// #define dataorder MSBFIRST
-// #define datamode SPI_MODE0 // Check this!!!!
-// /**
-//  * @brief    function to write a byte and read specified number of bytes from Adpd400 SPI
-//  * @param    pTxData: 8-bit register address and read bit pointer. Array of bytes
-//  * @param    pRxData: 8-bit data pointer. Array of bytes
-//  * @param    TxSize: 16-bit size of the data to write to the hardware
-//  * @param    RxSize: 16-bit size of the data to read from the hardware
-//  * @retval   status: 0 (Success), -1 (Error)
-//  */
-// uint16_t Adpd400x_SPI_Receive(uint8_t *pTxData, uint8_t *pRxData, uint16_t TxSize, uint16_t RxSize) {
-//     SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
-//     digitalWrite(BP_NSS, LOW); //enable device
+/**
+ * @brief Alternate LED1A and LED1B at two current values. 
+ * @param none
+ * @retval none
+ */
+void test_spi() {
+  // uint16_t green_reg = ADPD4x_REG_LED_POW12_A;
 
-//     for (int i = 0; i < (int)TxSize; i++) {
-//         SPI.transfer(pTxData[i]);
-//     }
 
-//     // The SPI protocol is based on a one byte OUT / one byte IN interface. 
-//     // For every byte expected to be received, one (dummy, typically 0x00 or 0xFF) byte must be sent.
-//     for (int i = 0; i < (int)RxSize; i++) {
-//         pRxData[i] = SPI.transfer(0x00);
-//     }
-    
-//     digitalWrite(BP_NSS, HIGH);
-//     SPI.endTransaction();
+  uint16_t ledRegAddress = ADPD4x_REG_LED_POW12_A; // timing slot A, I think
 
-//     return ((TxSize + RxSize) == sizeof(pTxData) + sizeof(pRxData))?ADI_OK:ADI_ERROR;
+  // hardcoded lol. see adi_adpd_slotops_helper for a more flexible function 
+  // in order: LED1A 50mA, LED1A 100mA, LED1B 50mA, LED1B 100mA
+  // uint16_t led_values[4] = {32U, 64U, 160U, 192U};
+  uint16_t led_values[2] = {8U, 16U};
 
-// }
+  // uint16_t led1A_50mA = 32U;
+  // uint16_t led1A_100mA = 64U;
+  // uint16_t led1B_50mA = 160U;
+  // uint16_t led1B_100mA = 192U;
 
-// uint16_t Adpd400x_SPI_Transmit(uint8_t *pTxData, uint16_t TxSize) {
-//     SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
-//     digitalWrite(BP_NSS, LOW); //enable device
+  // first 16 bits: 15 bit register address and 1 bit write command
+  uint16_t firsthalf = ((ledRegAddress) << 1U) | ADPD400x_SPI_WRITE; 
+  // if all bits are 0, then 0 current?
+  uint32_t led_off = ((firsthalf) << 16U) | (uint16_t) 0;
+  uint32_t buffer;
+  
+  int delay_ms = 200;
+  
+  SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
+  digitalWrite(BP_NSS, LOW); //enable device
 
-//     // Write register address and READ command
-//     for (int i = 0; i < TxSize; i++) {
-//         SPI.transfer(pTxData[i]);
-//     }
+  for (int i = 0; i < sizeof(led_values); i++) {
+    buffer = ((firsthalf) << 16U) | led_values[i];
+    SPI.transfer(&buffer, 4);
+    delay(delay_ms);
+    SPI.transfer(&led_off, 4);
+    delay(delay_ms);
+  }
+  digitalWrite(BP_NSS, HIGH);
+  SPI.endTransaction();
 
-//     digitalWrite(BP_NSS, HIGH);
-//     SPI.endTransaction();
+}
 
-//     return ADI_OK;
-    
-// }
+void read_chip_id() {
+
+  SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
+  digitalWrite(BP_NSS, LOW);
+  uint16_t buffer = ((ADPD4x_REG_CHIP_ID) << 1U) & ADPD400x_SPI_READ; 
+  SPI.transfer16(buffer);
+  SPI.transfer16(0x00);
+  digitalWrite(BP_NSS, HIGH);
+  SPI.endTransaction();
+
+}
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,46 +1,15 @@
 
 #include <SPI.h>
 #include "Arduino.h"
-#include "adi_adpd_driver.h"
-#include "adi_adpd_reg.h"
-#include "adi_adpd_ssm.h"
-#include "adi_adpd_slotops_helper.c"
+#include "../adi-adpd-library/adi_adpd_driver.h"
+#include "../adi-adpd-library/adi_adpd_reg.h"
+#include "../adi-adpd-library/adi_adpd_ssm.h"
+#include "../adi-adpd-library/adpd4000.h"
 #include "stdint.h" 
 // https://github.com/analogdevicesinc/adpd-drivers/blob/master/adpd40xx
 
-/* Bluepill pin values and settings */
-#define BP_LED_BUILTIN PC13 // Bluepill built-in LED
-#define BP_DATA_OUT PA7 // Bluepill MOSI 1
-#define BP_DATA_IN PA6 // Bluepill MISO 1
-#define BP_SCK PA5 // Bluepill SCK 1
-#define BP_NSS PA4 // Bluepill Chip Select 1
 
-/* spi 2 */
-// #define BP_DATA_OUT PB15 // Bluepill MOSI 2
-// #define BP_DATA_IN PB14 // Bluepill MISO 2
-// #define BP_SCK PB13 // Bluepill SCK 2
-// #define BP_NSS PB12 // Bluepill Chip Select 2
 
-/* arduino */
-// #define BP_LED_BUILTIN PB5
-// #define BP_DATA_OUT PB3 // Bluepill MOSI 2
-// #define BP_DATA_IN PB4  // Bluepill MISO 2
-// #define BP_SCK PB5 // Bluepill SCK 2
-// #define BP_NSS PB2 // Bluepill Chip Select 2
-#define BLINK_DUR_ms 200
-
-/* ADPD4000 SPI settings */
-// #define maxspeed 24000000 // ADPD4000 sclk frequency
-#define maxspeed 200000
-#define dataorder MSBFIRST
-#define datamode SPI_MODE0 // Check this!!!!
-
-//  #include <iostream> 
-// using namespace std;
-
-void blink_bp(int num_blinks);
-void test_spi();
-void read_chip_id();
 
 void setup() {
 
@@ -53,7 +22,6 @@ void setup() {
   SPI.setMISO(BP_DATA_IN);
   SPI.setMOSI(BP_DATA_OUT);
   SPI.setSCLK(BP_SCK);
-  SPI.setSSEL(BP_NSS);
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV128);
 
@@ -74,89 +42,14 @@ void loop() {
   
   read_chip_id();
   blink_bp(2);
-  delay(1000);
+  delay(200);
   
 
 }
 
-/**
- * @brief Blink the bluepill's builtin LED a number of times
- * @param num_blinks 
- * @retval none
- */
-void blink_bp(int num_blinks) {
-  for (int i=0; i < num_blinks; i++) {
-    delay(BLINK_DUR_ms);
-    digitalWrite(BP_LED_BUILTIN, LOW);
-    delay(BLINK_DUR_ms);
-    digitalWrite(BP_LED_BUILTIN, HIGH);
-  }
-}
-
-/**
- * @brief Alternate LED1A and LED1B at two current values. 
- * @param none
- * @retval none
- */
-void test_spi() {
-  // uint16_t green_reg = ADPD4x_REG_LED_POW12_A;
 
 
-  uint16_t ledRegAddress = ADPD4x_REG_LED_POW12_A; // timing slot A, I think
 
-  // hardcoded lol. see adi_adpd_slotops_helper for a more flexible function 
-  // in order: LED1A 50mA, LED1A 100mA, LED1B 50mA, LED1B 100mA
-  // uint16_t led_values[4] = {32U, 64U, 160U, 192U};
-  uint16_t led_values[2] = {8U, 16U};
-
-  // uint16_t led1A_50mA = 32U;
-  // uint16_t led1A_100mA = 64U;
-  // uint16_t led1B_50mA = 160U;
-  // uint16_t led1B_100mA = 192U;
-
-  // first 16 bits: 15 bit register address and 1 bit write command
-  uint16_t firsthalf = ((ledRegAddress) << 1U) | ADPD400x_SPI_WRITE; 
-  // if all bits are 0, then 0 current?
-  uint32_t led_off = ((firsthalf) << 16U) | (uint16_t) 0;
-  uint32_t buffer;
-  
-  int delay_ms = 200;
-  
-  SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
-  digitalWrite(BP_NSS, LOW); //enable device
-
-  for (int i = 0; i < sizeof(led_values); i++) {
-    buffer = ((firsthalf) << 16U) | led_values[i];
-    SPI.transfer(&buffer, 4);
-    delay(delay_ms);
-    SPI.transfer(&led_off, 4);
-    delay(delay_ms);
-  }
-  digitalWrite(BP_NSS, HIGH);
-  SPI.endTransaction();
-
-}
-
-void read_chip_id() {
-
-  SPI.beginTransaction(SPISettings(maxspeed, dataorder, datamode));
-  digitalWrite(BP_NSS, LOW);
-  delay(100);
-  uint16_t buffer = ((ADPD4x_REG_CHIP_ID) << 1U) & ADPD400x_SPI_READ; 
-  SPI.transfer16(buffer);
-  SPI.transfer16(0x00);
-  digitalWrite(BP_NSS, HIGH);
-  delay(100);
-  SPI.endTransaction();
-
-}
-
-void read_chip_id_library() {
-  uint16_t addr = ADPD4x_REG_CHIP_ID;
-  uint16_t pData = 0U;
-
-  adi_adpddrv_RegRead(addr, &pData);
-}
 
 
 
