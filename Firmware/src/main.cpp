d9a4e38f2f70272d5ba3f5511389ef7e30fb9645
gracemyz <gracemyzhang@gmail.com>
2024-02-08 23:58:12 -0800

    Reading from fifo

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -9,29 +9,7 @@
 // https://github.com/analogdevicesinc/adpd-drivers/blob/master/adpd40xx
 
 
-
-void setup () {
-  Serial.begin(9600);
-
-  // Set bluepill SPI pins
-  pinMode(BP_DATA_OUT, OUTPUT);
-  pinMode(BP_DATA_IN, INPUT);
-  pinMode(BP_SCK, OUTPUT);
-  pinMode(BP_NSS, OUTPUT);
-
-  SPI.setMISO(BP_DATA_IN);
-  SPI.setMOSI(BP_DATA_OUT);
-  SPI.setSCLK(BP_SCK);
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
-
-  pinMode(BP_LED_BUILTIN, OUTPUT);
-  // digitalWrite(BP_LED_BUILTIN, LOW);
-  // blink_bp(3);
-  digitalWrite(BP_NSS, HIGH); //disable ADPD4000 SPI
-
-  tAdiAdpdDcfgInst dcfg_ADPD4000[39] =
-{
+tAdiAdpdDcfgInst dcfg_ADPD4000[39] = {
   {0x0009U, 0x0085U}, // set high freq osc to half max frequency?
   {0x000bU, 0x02faU}, // set low freq osc to about 0.75 max
   {0x000fU, 0x0006U}, // use internal oscs. Use GPIO0 for alt clock. Use 1 MHz as low freq and enable it.
@@ -49,57 +27,72 @@ void setup () {
   {0x0110U, 0x0004U}, // data format
 };
 
-  tAdiAdpdDcfgInst single_integration_config[19] =
-  {
-    {0x0009U, 0x0085U}, // set high freq osc to half max frequency?
-    {0x000bU, 0x02faU}, // set low freq osc to about 0.75 max
-    {0x000fU, 0x0006U}, // use internal oscs. Use GPIO0 for alt clock. Use 1 MHz as low freq and enable it.
-    {0x000dU, 0x4E20U}, // low freq osc period set to 20000 (s?)
-    {0x0020U, 0x0004U}, // input pair in1 and in2 sleep state: in1 connected to vc1, in2 floating
-    {0x0021U, 0x0000U}, // input config: all inputs are single ended. vc1 and vc2 set to avdd during sleep
-    {ADPD4x_REG_TS_CTRL_A, 0x0000U}, // sample type A: default setting 0 for default sampling mode
-    {ADPD4x_REG_TS_PATH_A, 0x1DAU}, // signal path selection: TIA, BPF, integrator, and ADC.
-    {ADPD4x_REG_INPUTS_A, 0x0005U}, // 101: IN1 connected to channel 1; IN2 connected to channel 2; all else disconnected
-    {ADPD4x_REG_CATHODE_A, 0x5002U}, // precon anode to TIA_VREF, set 250 mV reverse bias across photodiode
-    // set TIA_VREF to 1.265 V;
-    // set TIA_VREF pulse alternate value also to 1.265 V (no pulses?), 
-    // set TIA channel 2 gain to 100 kOhm and TIA channel 1 gain also to 100 kOhm
-    {ADPD4x_REG_AFE_TRIM_A, 0x03C9U}, 
-    {ADPD4x_REG_LED_POW12_A, 0xAU}, // led settings: led1A to 16 mA
-    {ADPD4x_REG_PERIOD_A, 0U}, // TIA is continuously connected to input after precondition. No connection modulation.
-    {ADPD4x_REG_LED_PULSE_A, 0x219U}, // led pulse width 2us, first pulse offset 25 us
-    {ADPD4x_REG_INTEG_WIDTH_A, 0x3U}, // 3 us integration width, 1 ADC conversion per pulse 
-    {ADPD4x_REG_INTEG_OFFSET_A, 0x0206U}, // integ offset. Needs to be optimzed!
-    {ADPD4x_REG_COUNTS_A, 0x0105U}, // 5 pulses. 1 integration per ADC conversion.
-    {0x0010U, 0x0000U}, // operation mode idel
-    {0,0xFFFFU} // sentinel for end of loop???
-  };
+tAdiAdpdDcfgInst single_integration_config[19] =
+{
+  {0x0009U, 0x0085U}, // set high freq osc to half max frequency?
+  {0x000bU, 0x02faU}, // set low freq osc to about 0.75 max
+  {0x000fU, 0x0006U}, // use internal oscs. Use GPIO0 for alt clock. Use 1 MHz as low freq and enable it.
+  {0x000dU, 0x4E20U}, // low freq osc period set to 20000 (s?)
+  {0x0020U, 0x0004U}, // input pair in1 and in2 sleep state: in1 connected to vc1, in2 floating
+  {0x0021U, 0x0000U}, // input config: all inputs are single ended. vc1 and vc2 set to avdd during sleep
+  {ADPD4x_REG_TS_CTRL_A, 0x0000U}, // sample type A: default setting 0 for default sampling mode
+  {ADPD4x_REG_TS_PATH_A, 0x1DAU}, // signal path selection: TIA, BPF, integrator, and ADC.
+  {ADPD4x_REG_INPUTS_A, 0x0005U}, // 101: IN1 connected to channel 1; IN2 connected to channel 2; all else disconnected
+  {ADPD4x_REG_CATHODE_A, 0x5002U}, // precon anode to TIA_VREF, set 250 mV reverse bias across photodiode
+  // set TIA_VREF to 1.265 V;
+  // set TIA_VREF pulse alternate value also to 1.265 V (no pulses?), 
+  // set TIA channel 2 gain to 100 kOhm and TIA channel 1 gain also to 100 kOhm
+  {ADPD4x_REG_AFE_TRIM_A, 0x03C9U}, 
+  {ADPD4x_REG_LED_POW12_A, 0xAU}, // led settings: led1A to 16 mA
+  {ADPD4x_REG_PERIOD_A, 0U}, // TIA is continuously connected to input after precondition. No connection modulation.
+  {ADPD4x_REG_LED_PULSE_A, 0x219U}, // led pulse width 2us, first pulse offset 25 us
+  {ADPD4x_REG_INTEG_WIDTH_A, 0x3U}, // 3 us integration width, 1 ADC conversion per pulse 
+  {ADPD4x_REG_INTEG_OFFSET_A, 0x0206U}, // integ offset. Needs to be optimzed!
+  {ADPD4x_REG_COUNTS_A, 0x0155U}, // 105 = 5 pulses, 155=27 pulses?. 1 integration per ADC conversion.
+  {0x0010U, 0x0000U}, // operation mode idle
+  {0,0xFFFFU} // sentinel for end of loop???
+};
 
-  static tAdiAdpdSSmInst oAdiAppInst;
+static tAdiAdpdSSmInst oAdiAppInst;
 
-  // // ret = adi_adpdssm_SetLedCurrent(nUId,  nLed, 32U);
-  // adi_adpddrv_OpenDriver(); // probably not needed - for i2c and interrupts?
-  // adi_adpdssm_loadDcfg(dcfg_ADPD4000, 0x00U);
-  // adi_adpdssm_slotinit(&oAdiAppInst);
+
+void setup () {
+  Serial.begin(9600);
+
+  // Set bluepill SPI pins
+  pinMode(BP_DATA_OUT, OUTPUT);
+  pinMode(BP_DATA_IN, INPUT);
+  pinMode(BP_SCK, OUTPUT);
+  pinMode(BP_NSS, OUTPUT);
+
+  SPI.setMISO(BP_DATA_IN);
+  SPI.setMOSI(BP_DATA_OUT);
+  SPI.setSCLK(BP_SCK);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV128);
+
+  pinMode(BP_LED_BUILTIN, OUTPUT);
+  digitalWrite(BP_NSS, HIGH); //disable ADPD4000 SPI
+
+  // /* ADPD400x interrupt configuration instance */
+// InterruptIn adpd4x_interrupt(ADPD_INT_PIN);
+// EventFlags adpd4x_int_event;
+
 
   adi_adpddrv_OpenDriver();
+
   uint16_t ret = adi_adpdssm_loadDcfg(single_integration_config, 0xFFU);
   if (ret == ADI_ADPD_SSM_SUCCESS) {
     Serial.println("register config successful");
   } else {
     Serial.println("register config unsuccessful");
   }
-  delay(1000);
-
   ret = adi_adpdssm_slotinit(&oAdiAppInst);
   if (ret == ADI_ADPD_SSM_SUCCESS) {
     Serial.println("slot init successful");
   } else {
     Serial.println("slot init unsuccessful");
   }
-
-
-  delay(1000);
 
   ret = adi_adpdssm_setOperationMode(E_ADI_ADPD_MODE_SAMPLE);
   if (ret == ADI_ADPD_SSM_SUCCESS) {
@@ -112,11 +105,54 @@ void setup () {
 }
 
 void loop () {
-  
-  // blink_bp(2);
+  uint8_t aFifoDataBuf[MAX_SAMPLES_IN_FIFO];
+  uint16_t nAdpdFifoLevelSize;
+  ADI_ADPD_COMM_MODE bus_mode;
+  uint32_t adpd_ch1 = 0U;
+  uint16_t loop = 0U;
+  static uint32_t tick = 0U;
+    
 
+  while(1) {
+    /* Wait for FIFO_TH Interrupt, see ADPD400x_ISR
+    *  Wait forever for event '1', auto clear 
+    */
+    // adpd4x_int_event.wait_any(1U); 
+    uint32_t nRetValue = 0U;
+    // Not using ADPD400x auto clear int flag -> need to clear
+    adi_adpddrv_RegWrite(ADPD4x_REG_INT_STATUS_DATA, 0x8000);
+    /* Read the size of the data available in the FIFO */
+    nRetValue = adi_adpdssm_getFifoLvl(&nAdpdFifoLevelSize);
+    if (nRetValue != ADI_ADPD_DRV_SUCCESS)
+        continue;
+    /* Read the fifo data available in the FIFO */
+    nRetValue = adi_adpddrv_ReadFifoData(nAdpdFifoLevelSize, &aFifoDataBuf[0]);
+    if (nRetValue != ADI_ADPD_DRV_SUCCESS)
+        continue;
+
+    loop = 0U;
+    adpd_ch1 = 0U;
+    /* Read the data from the FIFO and print them */
+    while (nAdpdFifoLevelSize != 0u) {
+      
+        /* Byte swapping is needed to print ADPD data in proper format */
+        adpd_ch1 = ((aFifoDataBuf[loop] << 8) +
+                    (aFifoDataBuf[loop + 1]) +
+                    (aFifoDataBuf[loop + 2] << 24) +
+                    (aFifoDataBuf[loop + 3] << 16));
+        // Serial.print(nAdpdFifoLevelSize);
+        // Serial.print(",");
+        // Serial.print(tick);
+        
+        Serial.println(adpd_ch1);
+
+        nAdpdFifoLevelSize -= oAdiAppInst.oAdpdSlotInst.nTotalSlotSz;
+        loop += oAdiAppInst.oAdpdSlotInst.nTotalSlotSz;
+        tick += 1;
+    }
+  }
 }
-  
+
 
 
 
